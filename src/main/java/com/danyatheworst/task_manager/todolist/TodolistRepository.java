7a7bbe2a622e3727283ac19502169cc261d77079
danyatheworst <danyatheworst@gmail.com>
2024-10-11 12:09:14 +0300

    feature: fetching user's todolists

--- src/main/java/com/danyatheworst/task_manager/todolist/TodolistRepository.java
+++ src/main/java/com/danyatheworst/task_manager/todolist/TodolistRepository.java
@@ -1,5 +1,12 @@
 package com.danyatheworst.task_manager.todolist;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface TodolistRepository extends JpaRepository<Todolist, Long> { }
+import java.util.List;
+
+public interface TodolistRepository extends JpaRepository<Todolist, Long> {
+
+    @Query("SELECT t FROM Todolist t WHERE t.userId = :userId")
+    List<Todolist> findAllByUserId(Long userId);
+}
